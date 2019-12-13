3bade9d2337bd99d5c90bef14c2405538308bdd6
Jan-Olof Andersson <janoande02@student.kau.se>
2019-12-13 19:08:07 +0100

    Translate selection context menu options

--- src/background.js
+++ src/background.js
@@ -1,17 +1,47 @@
-
 "use strict";
 
-var langs = require('langs');
+const langs = require('langs');
 
 browser.runtime.onMessage.addListener(handleWordLookup);
 
 function handleWordLookup({ word, langcode }) {
-    let language = (langs.where("1", langcode) || { name: "English"}).name;
+    const language = (langs.where("1", langcode) || { name: "English"}).name;
     browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
-        let curTabIndex = tabs[0].index;
+        const curTabIndex = tabs[0].index;
         browser.tabs.create({
             url: "https://en.wiktionary.org/w/index.php?search=" + encodeURIComponent(word) + "#" + encodeURIComponent(language),
             index: curTabIndex + 1
         });
     });
 }
+
+// context menu for translating selected text
+
+browser.contextMenus.create({
+    id: "google-trans-selection",
+    title: "Translate with Google Translate",
+    contexts: ["selection"]
+});
+
+browser.contextMenus.create({
+    id: "deepl-trans-selection",
+    title: "Translate with DeepL",
+    contexts: ["selection"]
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    switch (info.menuItemId) {
+        case "google-trans-selection":
+            browser.tabs.create({
+                url: "https://translate.google.se/#view=home&op=translate&sl=auto&tl=en&text=" + encodeURIComponent(info.selectionText),
+                index: tab.index + 1
+            });
+        break;
+        case "deepl-trans-selection":
+            browser.tabs.create({
+                url: "https://www.deepl.com/translator#auto/en/" + encodeURIComponent(info.selectionText),
+                index: tab.index + 1
+            });
+        break;
+        }
+});
