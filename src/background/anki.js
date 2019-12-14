a60a5c23857e173955b4eee1f8843e4c35eec977
Jan-Olof Andersson <janoande02@student.kau.se>
2019-12-14 17:10:18 +0100

    Implement function for sending queries to anki-connect

---
+++ src/background/anki.js
@@ -0,0 +1,34 @@
+const url = "http://localhost:8765/";
+const ankiConnectVersion = 6;
+
+async function ankiQuery(action, params={}) {
+    try {
+        const query = {
+            "action" : action,
+            "version" : ankiConnectVersion,
+            "params": params
+        };
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(query),
+        });
+        const json = await response.json();
+        if (json.error)
+            throw json.error;
+        return json.result;
+    }
+    catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+async function ankiCheckVersion() {
+    const version = await ankiQuery("version");
+    if (version === ankiConnectVersion)
+        console.log("Anki-connect version", ankiConnectVersion, "found. OK");
+    else
+        console.log("Anki-connect version", version, " !=", ankiConnectVersion, "found. WARNING");
+}
+
+
+export { ankiCheckVersion };
