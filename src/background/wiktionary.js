9a99d32dc72691e5c643d15954de4697798c0ce0
Jan-Olof Andersson <janoande02@student.kau.se>
2019-12-17 21:47:59 +0100

    Handle ill-formatted wikitionary queries

--- src/background/wiktionary.js
+++ src/background/wiktionary.js
@@ -1,14 +1,16 @@
 async function wiktionaryQuery(word) {
+    word = word.replace(/\u00AD/g,''); // remove soft hyphens
+    word = word.replace(/ /g, "_");
     let url = "https://en.wiktionary.org/api/rest_v1/page/definition/" + encodeURIComponent(word);
     try {
         const response = await fetch(url);
+        if (!response.ok) throw response.status;
         const json = await response.json();
-        if (json.error)
-            throw json.error;
+        if (json.error) throw json.error;
         return json;
     }
     catch (error) {
-        console.error("Error:", error);
+        console.error("Wiktionary error:", error);
     }
 }
 
