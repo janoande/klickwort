64e96cdccc8c814254f38b9eca337acb5048db09
Jan-Olof Andersson <jan.o.andersson@outlook.com>
2020-01-13 18:54:58 +0100

    Fix packaging.

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,4 +1,4 @@
 import WordPopup from './wordPopup/wordPopup';
 import { h, render } from 'preact';
 
-render(<WordPopup />, document.body);
+render(<WordPopup />, document.body.appendChild(document.createElement('div')));
